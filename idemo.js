707a19f02d839c18cc4a94ffba00b2f81fea5f33
hajnalb1 <bencehajnal448@gmail.com>
2023-11-20 15:21:46 +0100

    first commit

---
+++ idemo.js
@@ -0,0 +1,16 @@
+
+
+const ember1 = {
+neve : 'a',
+kora :'20',
+magassaga : '150',
+sulyayyy:"lololo"
+};
+const ember2 = {
+neve : 'addd',
+kora :'2220',
+magassaga : '15110',
+sulyayyy:"lololsso"
+};
+let feladatlista = [ember1,ember2];
+console.log(ember1.neve)
